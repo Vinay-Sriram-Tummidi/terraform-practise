d9d20ff6024400c35b04485ab1fc2795e3e5224b
Vinay-Sriram-Tummidi <vinaysriram.tummidi@gmail.com>
2026-01-04 20:24:25 +0530

    Multi Region instance Creation

--- Task-1/main.tf
+++ Task-1/main.tf
@@ -1,12 +1,29 @@
+# Multile Region Resource Creation
+
+#default region
 provider "aws" {
   region = "ap-southeast-2"
+}
+
+#specific region
+provider "aws" {
+  alias = "region-1"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "instance-1" {
   ami           = "ami-0b8d527345fdace59"
   instance_type = "t3.micro"
-
   tags = {
     Name = "dev-ec2"
+  }
+}
+
+resource "aws_instance" "instance-2" {
+  ami           = "ami-068c0051b15cdb816"
+  instance_type = "t3.micro"
+  provider=aws.region-1
+  tags = {
+    Name = "test-ec2"
   }
 }
